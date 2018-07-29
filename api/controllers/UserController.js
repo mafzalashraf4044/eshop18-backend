6b90091d5a88159ec1dfbef7b7fe0278a94901af
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-07-29 12:56:43 +0500

    register user action added

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,4 +7,48 @@
 
 module.exports = {
 
+  registerUser: async (req, res) => {
+    /**
+     * Params:
+     * - firstName (req)
+     * - lastName (req)
+     * - username
+     * - email (req)
+     * - password (req)
+     * - country (req)
+     * - contactNumber (req)
+    */
+
+    sails.log('UsersController::registerUser called');
+
+    const params = req.allParams();
+    
+    /**
+     * Email field is unique for each user, user will be created
+     * only if no existing user has the same email address
+     * 
+     */
+
+    //  Feilds Pattern Validation
+    if (params.firstName && !/^[a-zA-Z][a-zA-Z]+[a-zA-Z]$/.test(params.firstName)) {
+      return res.json(400, {msg: 'First name is invalid.'});
+    } else if (params.lastName && !/^[a-zA-Z][a-zA-Z]+[a-zA-Z]$/.test(params.lastName)) {
+      return res.json(400, {msg: 'Last name is invalid.'});
+    } else if (params.email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(params.email)) {
+      return res.json(400, {msg: 'Email is invalid.'});
+    } else if (params.username && !/^[a-zA-Z0-9][a-zA-Z0-9]+[a-zA-Z0-9]$/.test(params.username)) {
+      return res.json(400, {msg: 'Username is invalid.'});
+    }
+
+    const user = await User.create(params)
+    .intercept('E_UNIQUE', (err)=> {
+      return 'Email already exists.';
+    }).intercept((err) => {
+      return err;
+    }).fetch();
+
+    return res.json(200, {user});
+
+  },
+  
 };
--- config/routes.js
+++ config/routes.js
@@ -40,4 +40,6 @@ module.exports.routes = {
   ***************************************************************************/
 
 
+  'POST /register': 'UserController.registerUser',
+
 };
