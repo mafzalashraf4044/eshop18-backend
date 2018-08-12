baad68be91bec453c76bb05ccea22a6e69f6d627
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-08-12 14:23:42 +0500

    update ecurrency commission helper added

---
+++ api/helpers/update-ecurrencies-commissions.js
@@ -0,0 +1,92 @@
+module.exports = {
+
+
+  friendlyName: 'Update Ecurrencies Commissions',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+    const eCurrencies = await Ecurrency.find({where: {isArchived: false}})
+    .intercept((err) => {
+      return err;
+    });
+
+    const paymentMethods = await PaymentMethod.find({where: {isArchived: false}})
+    .intercept((err) => {
+      return err;
+    });
+
+    let commissions = [
+      ...eCurrencies.map(eCurrency => ({
+          title: eCurrency.title, percentage: 0, fixed: 0
+      })),
+      ...paymentMethods.map(paymentMethod => ({
+          title: paymentMethod.title, percentage: 0, fixed: 0
+      }))
+    ];
+
+    _.forEach(eCurrencies, async (eCurrency, index) => {
+      
+      // buy commission
+      const newBuyCommissions = _.filter(commissions, (commission) => (eCurrency.title !== commission.title));
+      
+      _.forEach(eCurrency.buyCommissions, buyCommission => {
+        const index = _.findIndex(newBuyCommissions, (newBuyCommission) => (newBuyCommission.title === buyCommission.title));
+
+        if (index !== -1) {
+          newBuyCommissions[index] = buyCommission;
+        }
+      });
+
+      // sell commission
+      let newSellCommissions = _.filter(commissions, (commission) => (eCurrency.title !== commission.title));
+
+      _.forEach(eCurrency.sellCommissions, sellCommission => {
+        const index = _.findIndex(newSellCommissions, (newSellCommission) => (newSellCommission.title === sellCommission.title));
+
+        if (index !== -1) {
+          newSellCommissions[index] = sellCommission;
+        }
+      });
+
+      // exchange commission
+      let newExchangeCommissions = _.filter(commissions, (commission) => (eCurrency.title !== commission.title));
+
+      _.forEach(eCurrency.exchangeCommissions, exchangeCommission => {
+        const index = _.findIndex(newExchangeCommissions, (newExchangeCommission) => (newExchangeCommission.title === exchangeCommission.title));
+
+        if (index !== -1) {
+          newExchangeCommissions[index] = exchangeCommission;
+        }
+      });
+
+      const updateECurrency = await Ecurrency.update({id: eCurrency.id}, {
+        buyCommissions: newBuyCommissions,
+        sellCommissions: newSellCommissions,
+        exchangeCommissions: newExchangeCommissions,
+      }).intercept((err) => {
+        return err;
+      }).fetch();    
+      
+    });
+
+    // All done.
+    return exits.success();
+
+  }
+
+
+};
+
